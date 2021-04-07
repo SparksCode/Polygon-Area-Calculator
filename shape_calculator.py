50730418eff9338905832c4d5916a0cbdc790b09
Zachary Sparks <>
2021-04-07 22:05:54 +0000

    Create skeletons of both the Rectangle and Square class.

--- shape_calculator.py
+++ shape_calculator.py
@@ -1,6 +1,34 @@
 class Rectangle:
+  def __init__(self, height, width):
+    self.height = height
+    self.width = width
 
+  def set_height(self, height):
+    return ""
 
+  def set_width(self, width):
+    return ""
 
+  def get_amount_inside(self):
+    return ""
 
-class Square:
+  def get_area(self):
+    return ""
+
+  def get_diagonal(self):
+    return ""
+
+  def get_perimeter(self):
+    return ""
+
+  def get_picture(self):
+    return ""
+
+class Square(Rectangle):
+
+  def __init__(self, width):
+    self.height = width
+    self.width = width
+
+  def set_side(self, width):
+    return ""
