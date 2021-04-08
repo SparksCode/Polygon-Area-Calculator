62e5a707bd795a6c6749383013c54fabfcdfe81e
Zachary Sparks <>
2021-04-08 15:21:39 +0000

    Complete story requirements. Passes all tests.

--- shape_calculator.py
+++ shape_calculator.py
@@ -1,28 +1,38 @@
 class Rectangle:
-  def __init__(self, height, width):
+  def __init__(self, width, height):
     self.height = height
     self.width = width
 
+  def __str__(self):
+    return f"Rectangle(width={self.width}, height={self.height})"
+
   def set_height(self, height):
-    return ""
+    self.height = height
 
   def set_width(self, width):
-    return ""
+    self.width = width
 
-  def get_amount_inside(self):
-    return ""
+  def get_amount_inside(self, rectangle):
+    if rectangle.get_area() > self.get_area():
+      return 0
+    return self.get_area() // rectangle.get_area()
 
   def get_area(self):
-    return ""
+    return self.width * self.height
 
   def get_diagonal(self):
-    return ""
+    return ((self.width ** 2 + self.height ** 2) ** .5)
 
   def get_perimeter(self):
-    return ""
+    return (2 * self.width + 2 * self.height)
 
   def get_picture(self):
-    return ""
+    picture = ""
+    if self.height > 50 or self.width > 50:
+      return "Too big for picture."
+    for i in range(0, self.height, 1):
+      picture += "*" * self.width + "\n"
+    return picture
 
 class Square(Rectangle):
 
@@ -30,5 +40,8 @@ class Square(Rectangle):
     self.height = width
     self.width = width
 
+  def __str__(self):
+    return f"Square(side={self.width})"
   def set_side(self, width):
-    return ""
+    self.height = width
+    self.width = width
